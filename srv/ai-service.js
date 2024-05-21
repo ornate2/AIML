a3afdfb3d1d0d280bcc41a658e1dbda99b0b4ff3
Amit Gupta <amit.g20@outlook.com>
2024-05-21 14:31:06 +0000

    Predict api call

--- srv/ai-service.js
+++ srv/ai-service.js
@@ -1,7 +1,28 @@
 
 module.exports = cds.service.impl(async function (srv) {
     srv.on('POST', 'Predict', async (req) => {
-        
-        return req.data;
+
+        try {
+            let payload = {
+                "Error Description": req.data.description
+            };
+            const predictApi = await cds.connect.to('Predict_API');
+
+            const predictResponse = await predictApi.send({
+                method: 'POST',
+                path: `/predict`,
+                data: payload,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            
+            return {
+                description: JSON.stringify(predictResponse)
+            };
+        } catch (error) {
+            console.error(error.message);
+            req.error(500, error.message);
+        }
     });
 });
